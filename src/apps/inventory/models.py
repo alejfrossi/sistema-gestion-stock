6c61079a6c3faf8d6879bb5ae1ab514f27162ac1
Alejo <alejfrossi.uni@gmail.com>
2026-01-15 11:40:58 -0300

    feat(inventory): implement total value calculation in product list

--- src/apps/inventory/models.py
+++ src/apps/inventory/models.py
@@ -40,3 +40,7 @@ class Product(models.Model):
 
     def __str__(self):
         return f"{self.name} (${self.price})"
+    
+    @property
+    def total_value(self):
+        return self.price * self.quantity
